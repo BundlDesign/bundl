a595edc7a1b1c2a48c762b9473c9c1c7ac5aab48
Jens Deryckere <97600411+jery-jens@users.noreply.github.com>
2023-02-20 10:36:35 +0100

    First draft of code

---
+++ phases.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll(".phase-services-link");
+    const phases = document.querySelectorAll(".phases-service-item");
+
+    links.forEach((link, i) => {
+        link.addEventListener("click", () => {
+            const coordinates = phases[i].getBoundingClientRect();
+            
+            window.scrollTo({
+                top: coordinates.top,
+                behavior: "smooth",
+            });
+        });
+    });
+});
